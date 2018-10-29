8ed73256be50b3f6257ee80c5307808becce97f9
Christa <christadcooke@gmail.com>
2018-10-28 19:23:28 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { getAsteroids } from './Routes.js';
 import Asteroid from './Asteroid';
+import { Jumbotron, Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
@@ -12,31 +13,31 @@ class App extends Component {
     };
   }
 
-componentDidMount(){
-  var d = new Date();
-  var month = d.getMonth()+1;
-  var promise = getAsteroids(d.getFullYear()+'-'+month+'-'+d.getDate());
-  promise.then((asteroids) =>  this.setState({asteroids}));
-}
+  componentDidMount() {
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var promise = getAsteroids(d.getFullYear() + '-' + month + '-' + d.getDate());
+    promise.then((asteroids) => this.setState({ asteroids }));
+  }
 
-displayAsteroids(asteroids) {
-  return (
-    <ul>
-      {asteroids.map((a) =>
-        <Asteroid key={a.id}
-              name={a.name}
-              hazardous={a.is_potentially_hazardous_asteroid}
-              approachDate={a.close_approach_data[0].close_approach_date}
-              missDistance={a.close_approach_data[0].miss_distance.kilometers}
-              />
-      )}
-    </ul>
-  );
-}
+  displayAsteroids(asteroids) {
+    return (
+      <ul>
+        {asteroids.map((a) =>
+          <Asteroid key={a.id}
+            name={a.name}
+            hazardous={a.is_potentially_hazardous_asteroid}
+            approachDate={a.close_approach_data[0].close_approach_date}
+            missDistance={a.close_approach_data[0].miss_distance.kilometers}
+          />
+        )}
+      </ul>
+    );
+  }
 
   render() {
-    if(!this.state.asteroids){
-      return(
+    if (!this.state.asteroids) {
+      return (
         <div className="App">
           <p>Loading</p>
         </div>
@@ -44,25 +45,16 @@ displayAsteroids(asteroids) {
     }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <div>
-        <ul>
-          {this.displayAsteroids(this.state.asteroids)}
-        </ul>
-        </div>
+        <Jumbotron>
+          <h1 className="display-3">Near Earth Objects</h1>
+          <p className="lead">Space objects and asteroids approaching Earth</p>
+          <hr className="my-2" />
+        </Jumbotron>
+        <Container>
+          <Row>
+            <Col>{this.displayAsteroids(this.state.asteroids)}</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
--- src/Asteroid.js
+++ src/Asteroid.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import './Asteroid.css';
 
 class Asteroid extends Component {
-
   render() {
+    let hazardous = '';
+    if(this.props.hazardous){
+      hazardous = 'true';
+    } else {
+      hazardous = 'false';
+    }
+  
     return (
       <div className="Asteroid">
-        <p> {this.props.name} </p>
-        <p> {this.props.hazardous} </p>
-        <p> {this.props.approachDate} </p>
-        <p> {this.props.missDistance} </p>
+        <p> Name: {this.props.name} </p>
+        <p> Is Hazardous: {hazardous} </p>
+        <p> Approach Date: {this.props.approachDate} </p>
+        <p> Miss Distance: {this.props.missDistance} </p>
       </div>
     );
   }
